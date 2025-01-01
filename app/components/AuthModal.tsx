9c9f79b645bbb25886a298d6e99c59276090131b
emmanuellaAk <emmanuellaanimwaa17@icloud.com>
2025-01-01 23:11:32 +0000

    auth,ui,orm

---
+++ app/components/AuthModal.tsx
@@ -0,0 +1,15 @@
+import { Button } from "@/components/ui/button";
+import { Dialog, DialogTrigger } from "@/components/ui/dialog";
+
+export function AuthModal() {
+  return(
+  <Dialog>
+     <DialogTrigger>
+      <Button> 
+         Try For Free
+      </Button>
+     
+    </DialogTrigger>
+  </Dialog>
+  )
+}
---
+++ app/components/Navbar.tsx
@@ -0,0 +1,15 @@
+import Link from 'next/link'
+import Image from 'next/image'
+import Planrr from '@/public/planrrrr.png'
+import { AuthModal } from './AuthModal'
+export function Navbar() {
+  return (
+    <div className="flex py-5 items-center justify-between ">
+     <Link href="/" className='flex items-center'>
+       {/* <Image src={Planrr} alt='Logo' className='size-10'/> */}
+       <h4 className=' text-3xl font-semibold'>Plan<span className='text-blue-500'>RR</span></h4>
+     </Link>
+     <AuthModal/>
+    </div>
+  )
+}
